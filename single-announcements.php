9a39770d848a3946893a95f9cf5dd8f6dd7636bc
Abhishek <abhiverma.abhishek@gmail.com>
2018-12-05 16:07:26 +0530

    update from current sever repo

--- single-announcements.php
+++ single-announcements.php
@@ -67,6 +67,17 @@ ob_start();
                                     <script type="text/javascript" src="http://s7.addthis.com/js/200/addthis_widget.js"></script>
                                 </div>
                             </div>
+                        <?php }else if(get_the_ID() == '17460' ){ ?>
+                            <div class="shortheading-pdflink">
+                                <div class="heading">
+                                    <h3><?php the_title(); ?></h3>
+                                </div>
+                                <div class="share-pdflink">
+                                    <a  href="<?php echo site_url(); ?>/wp-content/uploads/2018/10/two-openings-at-fmes-ijme.pdf" target="_blank" title = "Download PDF" > <img src="<?php echo THEME_URL; ?>/images/download-pdf.png" width="95" height="16" border="0" alt="Bookmark and Share" style="border:0;padding:0" > </a>
+                                    <a href="http://www.addthis.com/bookmark.php" onmouseover="return addthis_open(this, '', '<?php echo $addExtraString; ?>?@ijme.in', '<?php echo get_the_title(); ?>')" onmouseout="addthis_close()" onclick="return addthis_sendto()"> <img src="http://s7.addthis.com/static/btn/lg-share-en.gif" width="125" height="16" border="0" alt="Bookmark and Share" style="border:0;padding:0"> </a>
+                                    <script type="text/javascript" src="http://s7.addthis.com/js/200/addthis_widget.js"></script>
+                                </div>
+                            </div>
                         <?php }else if( get_the_ID() == '16832'){ ?>
 							<div class="shortheading-pdflink">
 								<div class="heading">
