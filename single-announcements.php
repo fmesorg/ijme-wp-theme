cbe0f02d039a7053fa2367dc266bec967b7135eb
Abhishek <abhiverma.abhishek@gmail.com>
2019-01-15 12:00:32 +0530

    added pdf download and share button for announcement

--- single-announcements.php
+++ single-announcements.php
@@ -78,6 +78,17 @@ ob_start();
                                     <script type="text/javascript" src="https://s7.addthis.com/js/200/addthis_widget.js"></script>
                                 </div>
                             </div>
+                        <?php }else if(get_the_ID() == '17708' ){ ?>
+                            <div class="shortheading-pdflink">
+                                <div class="heading">
+                                    <h3><?php the_title(); ?></h3>
+                                </div>
+                                <div class="share-pdflink">
+                                    <a  href="<?php echo site_url(); ?>/wp-content/uploads/2019/01/dr-richard-ash-grandround-invitation.pdf" target="_blank" title = "Download PDF" > <img src="<?php echo THEME_URL; ?>/images/download-pdf.png" width="95" height="16" border="0" alt="Bookmark and Share" style="border:0;padding:0" > </a>
+                                    <a href="http://www.addthis.com/bookmark.php" onmouseover="return addthis_open(this, '', '<?php echo $addExtraString; ?>?@ijme.in', '<?php echo get_the_title(); ?>')" onmouseout="addthis_close()" onclick="return addthis_sendto()"> <img src="https://s7.addthis.com/static/btn/lg-share-en.gif" width="125" height="16" border="0" alt="Bookmark and Share" style="border:0;padding:0"> </a>
+                                    <script type="text/javascript" src="https://s7.addthis.com/js/200/addthis_widget.js"></script>
+                                </div>
+                            </div>
                         <?php }else if( get_the_ID() == '16832'){ ?>
 							<div class="shortheading-pdflink">
 								<div class="heading">
