0264622ba0818b63c6fe52ab7b68fe7df82389c8
Abhishek <abhiverma.abhishek@gmail.com>
2019-05-07 13:26:12 +0530

    adding missing from prod

--- nav.php
+++ nav.php
@@ -1,7 +1,8 @@
 <nav id="site-navigation" class="navbar navbar-default" role="navigation">
-    <div class="navheader">  
+    <div class="navheader">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-mobile">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-mobile">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -10,80 +11,84 @@
             <a class="navbar-brand" href="#"><?php bloginfo( 'name' ); ?></a>
         </div>
         <!--<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', '_s' ); ?></a>-->
-		<div class="hidden-xs hidden-sm">
-        <?php
+        <div class="hidden-xs hidden-sm">
+            <?php
             wp_nav_menu( array(
                 //'menu'              => 'primary',
-                'theme_location'    => 'main',
-                'depth'             => 4,
-                'container'         => 'div',
-                'container_class'   => 'collapse navbar-collapse dropnav',
-                'container_id'      => 'bs-example-navbar-collapse-1',
-                'menu_class'        => 'nav navbar-nav',
-                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                'walker'            => new wp_bootstrap_navwalker())
+            'theme_location' => 'main',
+            'depth' => 4,
+            'container' => 'div',
+            'container_class' => 'collapse navbar-collapse dropnav',
+            'container_id' => 'bs-example-navbar-collapse-1',
+            'menu_class' => 'nav navbar-nav',
+            'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+            'walker' => new wp_bootstrap_navwalker())
             );
-        ?>
-		</div>
-		
-		<div class="visible-xs visible-sm">
-		<?php
+            ?>
+        </div>
+
+        <div class="visible-xs visible-sm">
+            <?php
             wp_nav_menu( array(
                 'menu'              => 'Mobile Main Menu',
-                //'theme_location'    => 'header',
-                'depth'             => 4,
-                'container'         => 'div',
-                'container_class'   => 'collapse navbar-collapse dropnav',
-                'container_id'      => 'bs-example-navbar-collapse-mobile',
-                'menu_class'        => 'nav navbar-nav',
-                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                'walker'            => new wp_bootstrap_navwalker())
+            //'theme_location' => 'header',
+            'depth' => 4,
+            'container' => 'div',
+            'container_class' => 'collapse navbar-collapse dropnav',
+            'container_id' => 'bs-example-navbar-collapse-mobile',
+            'menu_class' => 'nav navbar-nav',
+            'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+            'walker' => new wp_bootstrap_navwalker())
             );
-        ?>
-		</div>
+            ?>
+        </div>
         <?php
         /*
-        wp_nav_menu( array( 
+        wp_nav_menu( array(
                 'theme_location' 	=> 'main',
-                'container' 		=> 'div',
-                'container_class' 	=> 'collapse navbar-collapse',
-                'container_id'    	=> 'main-navbar-collapse',
-                'menu_class'      	=> 'nav navbar-nav',
-                'menu_id'         	=> '',
-                'echo'            	=> true,
-                'fallback_cb'     	=> 'wp_page_menu',
-                'before'          	=> '',
-                'after'           	=> '',
-                'link_before'     	=> '',
-                'link_after'      	=> '',
-                'items_wrap'      	=> '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                'depth'           	=> 0,
-                'walker'          	=> ''
+        'container' => 'div',
+        'container_class' => 'collapse navbar-collapse',
+        'container_id' => 'main-navbar-collapse',
+        'menu_class' => 'nav navbar-nav',
+        'menu_id' => '',
+        'echo' => true,
+        'fallback_cb' => 'wp_page_menu',
+        'before' => '',
+        'after' => '',
+        'link_before' => '',
+        'link_after' => '',
+        'items_wrap' => '
+        <ul id="%1$s" class="%2$s">%3$s</ul>
+        ',
+        'depth' => 0,
+        'walker' => ''
         ));
         */
         ?>
-		<ul class="search_box">
-			<a href="#" class="search-icon"><span class="search_icon"> </span></a>
-			<form method="get" action="<?php echo site_url(); ?>" id="searchform" class="search-form" role="search" style="display: none;">
-				<div class="input-group add-on">
-					<input class="form-control" placeholder="Search" name="s" id="srch-term" type="text">
-				<div class="input-group-btn">
-					<button class="btn search-submit" type="submit">search</button>
-				</div>
-				</div>
-			</form>
-		</ul>
+        <ul class="search_box">
+            <a href="#" class="search-icon"><span class="search_icon"> </span></a>
+            <form method="get" action="<?php echo site_url(); ?>" id="searchform" class="search-form" role="search"
+                  style="display: none;">
+                <div class="input-group add-on">
+                    <input class="form-control" placeholder="Search" name="s" id="srch-term" type="text">
+                    <div class="input-group-btn">
+                        <button class="btn search-submit" type="submit">search</button>
+                    </div>
+                </div>
+            </form>
+        </ul>
     </div>
-	
+
 </nav><!-- #site-navigation -->
-<!--
 <?php if(get_the_ID() == '16406' ){ ?>
-	<?php $post   = get_post( 16974 );  ?>
-	<div class="marquee-container" onmouseover="this.stop();" onmouseout="this.start();">
-	<span class="red-badge">Announcements</span>
-	<marquee onmouseover="this.stop();" onmouseout="this.start();" class="header-marquee">
-	<img src="http://ijme.in/images/tag-new-icon.gif"> <a href="http://ijme.in/announcements/position-intern-office-support-staff-fmes-ijme/" target="_blank">POSITION: INTERN / OFFICE SUPPORT STAFF, FMES-IJME</a>&nbsp;&nbsp;|&nbsp;&nbsp;<img src="http://ijme.in/images/tag-new-icon.gif"> <a href="https://iab-secretariat.org/wp/" target="_blank">Don't forget to pay your IAB membership fees – necessary for congress participation!</a>&nbsp;&nbsp;|&nbsp;&nbsp;<img src="http://ijme.in/images/tag-new-icon.gif"> <a href="http://ijme.in/announcements/two-openings-at-fmes-indian-journal-of-medical-ethics/" target="_blank">Two openings at FMES-Indian Journal of Medical Ethics</a>&nbsp;&nbsp;|&nbsp;&nbsp; <img src="http://ijme.in/images/tag-new-icon.gif"> <a href="/nbc-20140321/index.php/14th-wcb-india/index/pages/view/registration" target="_blank">Early bird date for registrations extended to 15 October, 2018</a>&nbsp;&nbsp;|&nbsp;&nbsp;<img src="http://ijme.in/images/tag-new-icon.gif"> <a href="/nbc-20140321/index.php/14th-wcb-india/index/pages/view/bursary" target="_blank">Last date for bursary applications extended to October 5, 2018!</a>&nbsp;&nbsp;|&nbsp;&nbsp; <a href="http://ijme.in/announcement_call_for_nominations_ijme_ethics_award_2018" target="_blank">IJME Ethics Award 2018: Last date for sending nominations extended to 15 September 2018!</a>&nbsp;&nbsp;|&nbsp;&nbsp; <a href="http://ijme.in/nbc-20140321/index.php/14th-wcb-india/index/pages/view/online-payment" target="_blank">Congress registrations are live!</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="http://ijme.in/nbc-20140321/index.php/14th-wcb-india/index/pages/view/unravelling-bio-markets" target="_blank">Pre-Congres session: Unravelling Bio-markets through the lens of ethics gender, race and political economy</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="http://ijme.in/announcements/three-openings-at-fmes-bioethics-centre-enabled-and-seeded-by-tata-trusts/" target="_blank">Vacancies at the all new FMES Bioethics Centre</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="/../../nbc-20140321/index.php/14th-wcb-india/index/pages/view/accommodation" target="_blank">Joint 14th WCB and 7th NBC: Updated list of accommodations for Congress registrants to consider</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="http://ijme.in/nbc-20140321/index.php/14th-wcb-india/index/pages/view/writing-case-reports-for-bioethics-journals" target="_blank">14th WCB and 7th NBC: Pre-congress session on Writing Case Reports for Bioethics Journal</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="http://ijme.in/nbc-20140321/index.php/14th-wcb-india/index/pages/view/world-congress-of-bioethics-bengaluru-2018" target="_blank">14th WCB and 7th NBC: Pre-Congress session on Organ Donation and Transplantation in a world of inequality</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="/nbc-20140321/index.php/14th-wcb-india/index/pages/view/pediatric-bioethics-in-india" target="_blank">14th WCB and 7th NBC: Pre-congress session on Pediatrics Bioethics in India</a>
-	</marquee>
-	</div>
+<?php $post   = get_post( 16974 );  ?>
+<div class="marquee-container" onmouseover="this.stop();" onmouseout="this.start();">
+    <span class="red-badge">Announcements</span>
+    <marquee onmouseover="this.stop();" onmouseout="this.start();" class="header-marquee">
+        <img src="http://ijme.in/images/tag-new-icon.gif"> <a
+            href="https://ijme.in/announcements/vacancy-announcement-editorial-coordinatorwebsite-manager-at-ijme/"
+            target="_blank">Vacancy announcement: EDITORIAL COORDINATOR/WEBSITE MANAGER at IJME</a>&nbsp;
+        
+    </marquee>
+</div>
 <?php } ?>
--->
--- single-announcements.php
+++ single-announcements.php
@@ -133,7 +133,17 @@ ob_start();
 									<script type="text/javascript" src="https://s7.addthis.com/js/200/addthis_widget.js"></script>
 								</div>
 							</div>
-							
+                        <?php }else if( get_the_ID() == '17851'){ ?>
+                            <div class="shortheading-pdflink">
+                                <div class="heading">
+                                    <h3><?php the_title(); ?></h3>
+                                </div>
+                                <div class="share-pdflink">
+                                    <a  href="<?php echo site_url(); ?>/wp-content/uploads/2019/03/IJME_Ed-coordinator-Website-Manager_ADVERT.pdf" target="_blank" title = "Download PDF" > <img src="<?php echo THEME_URL; ?>/images/download-pdf.png" width="95" height="16" border="0" alt="Bookmark and Share" style="border:0;padding:0" > </a>
+                                    <a href="http://www.addthis.com/bookmark.php" onmouseover="return addthis_open(this, '', '<?php echo $addExtraString; ?>?@ijme.in', '<?php echo get_the_title(); ?>')" onmouseout="addthis_close()" onclick="return addthis_sendto()"> <img src="https://s7.addthis.com/static/btn/lg-share-en.gif" width="125" height="16" border="0" alt="Bookmark and Share" style="border:0;padding:0"> </a>
+                                    <script type="text/javascript" src="https://s7.addthis.com/js/200/addthis_widget.js"></script>
+                                </div>
+                            </div>
 						<?php }else { ?>				
 							<h3><?php the_title(); ?></h3>
 						<?php } ?>
