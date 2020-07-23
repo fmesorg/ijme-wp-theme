744a23014f5f5a6f73a37a931f0fc0fb16bb0f3a
Abhishek Verma <abhi11verma@gmail.com>
2020-07-23 16:06:23 +0530

    ADD : Link Blog post to homepage

--- template-parts/home-page/home-blog.php
+++ template-parts/home-page/home-blog.php
@@ -4,20 +4,38 @@
      */
 ?>
 
+<?php
+    global $post;
+    $blogs = get_posts(array(
+      'posts_per_page' => 3,
+      'post_type' => 'blog',
+    ));
+?>
+
 <div id="blog" class="blog-wrapper col">
     <div id="blog-section-title" class="dp-flex">
         Blog
         <div class="view-all-wrapper"><span class="view-all-text">VIEW ALL</span> <span class="view-all-icon">></span></div>
     </div>
     <div class="blog-item-wrapper">
-        <div class="blog-item">
-            <div class="blog-description">
-                George Thomas Draft New Education Policy offers contentious remedies for a structural malady in medical studies.
-            </div>
-            <div style="margin-top: 13px">
-                <span class="blog-item-author">The Hindu</span>
-                <span class="blog-item-date">25 Jul 2019</span>
-            </div>
-        </div>
+        <?php
+            if ($blogs) {
+                foreach ($blogs as $post) :
+                    setup_postdata($post); ?>
+                    <div class="blog-item">
+                        <div class="blog-description">
+                            <?php echo get_the_title(); ?>
+                        </div>
+                        <div class="blog-item-details">
+<!--       Author hidden as not required by FMES Team  <span class="blog-item-author">The Hindu</span>-->
+                            <span class="blog-item-date"><?php echo date("F j, Y", strtotime($post->post_date)); ?></span>
+                        </div>
+                    </div>
+                <?php endforeach;
+                wp_reset_postdata();
+    
+            }
+        ?>
     </div>
+
 </div>
