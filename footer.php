8a407526d94544f270a961216e88302859a550e5
Abhishek Verma <abhi11verma@gmail.com>
2020-08-04 16:03:31 +0530

    ADD : Logos to footer

--- footer.php
+++ footer.php
@@ -3,6 +3,25 @@
         
         <div class="footer">
             <?php  get_template_part('template-parts/nav-footer'); ?>
+            <div id="sub-footer">
+                <div>
+                    <a><img width="100" src="<?php echo THEME_URL; ?>/images/logo.png" alt="Page Footer IJME Logo"></a>
+                    <div class="footer-brand-text">PUBLISHED BY FORUM FOR MEDICAL ETHICS SOCIETY SINCE 1993</div>
+                    <div class="footer-brand-text">ONLINE ISSN: 0975-5091 PRINT ISSN: 0974-8466</div>
+                </div>
+                <div>
+                    <img src="<?php echo THEME_URL; ?>/images/footer/heal_logo.svg"
+                         alt="HEAL Logo">
+                </div>
+                <div>
+                    <img src="<?php echo THEME_URL; ?>/images/footer/nbc_logo.svg"
+                         alt="NBC Logo">
+                </div>
+                <div>
+                    <img src="<?php echo THEME_URL; ?>/images/footer/fmes_logo.svg"
+                         alt="FMES Logo">
+                </div>
+            </div>
         </div>
 
         <?php
