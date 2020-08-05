a49b0e2955537af55d3673b2cc6f49c19d9282cf
Abhishek Verma <abhi11verma@gmail.com>
2020-08-05 21:13:45 +0530

    Fix : Common page

--- home.php
+++ home.php
@@ -12,22 +12,7 @@ global $cat_editorial_id;
 ?>
 
 <div id="main">
-<!-- <div class="row">
-	<div class="col-sm-12">
-		<div class="ticker-container">
-			<div class="ticker-sibling">
-				Announcements
-			</div>
-			<div class="ticker-text">
-				<span><a href="/nbc-20140321/index.php/NBC-6/index/pages/view/registration" target="_blank">Registration</a> for Sixth National Bioethics Conference Open</span><span>Announcements of <a href="/nbc-20140321/index.php/NBC-6/index/pages/view/announcement-of-nsf" target="_blank">pre-conference workshop</a> on transition of care in patients with advanced and life limiting conditions. Please sign up</span><span>Announcement of a <a href="/nbc-20140321/index.php/NBC-6/index/pages/view/visit-to-cipla-palliative-care-and-training-centre" target="_blank">Study Tour</a> to the Cipla Palliative Care and Training Centre, Pune on Jan 12, 2017 and Jan 15, 2017.</span>
-			</div>
-		</div>
-	</div>
-</div>-->
 <div id="content">
-
-
-
     <div>
 	<?php the_content(); ?>
 	</div>
--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php get_header(); ?>
 
 <div class="row">
-    <div class="dp-flex margin-lr-10">
-        <div class="col-md-9 blocks margin-r-5">
             <?php
             if ( have_posts() ) {
                 while ( have_posts() ) {
                     the_post();
-
                     /*
                     if(is_search()) {
                         get_template_part( 'search' );
@@ -18,45 +15,22 @@
                         get_template_part( 'home' );
                         break;
                     }
+    
                     ?>
-                    <?php if(!(get_post_type() == 'articles' && isset($_GET['galley']) && $_GET['galley'] == 'html')) { ?>
-
-                            <div id="breadcrumb">
-                                <!--a href="<?php //echo site_url(); ?>" class="current" target="_parent">Home</a-->
-                                <?php $uri = $_SERVER['REQUEST_URI'];
-                                 ?>
-                            </div>
-
-                    <?php } ?>
-
-                    <div id="content">
-                        <?php if (get_post_type() == 'articles') {
-
-                            if(isset($_GET['galley']) && $_GET['galley'] == 'pdf') {
-                                $pdf_file = get_post_meta(get_the_ID(),'pdf_file',true);
-                                if( $pdf_file ) {
-                                ?>
-                                <div id="pdf-wrap"></div>
-                                <script>PDFObject.embed('<?php echo $pdf_file; ?>', "#pdf-wrap");</script>
-                                <?php
-                                }
-                            }
-                        }
-                        ?>
-
-                        <?php if(get_post_type() == 'page') the_content(); ?>
+                    <div id="page-content-wrapper">
+                        <?php if (get_post_type() == 'page') the_content(); ?>
+        
+                        <?php if (get_post_type() == 'post') { ?>
+                            <div class="post-title"><?php the_title(); ?> </div>
+                            <div class="post-date">Date of Publication: <?php echo get_the_date('Y-m-d'); ?> </div>
+                            <div class="post-content"><?php the_content(); ?> </div>
+                            <?php
+                        } ?>
                     </div>
                     <?php
-
-
+                }
             } // end if
             ?>
         </div>
-        <div class="col-md-3 blocks">
-            <?php get_sidebar(); ?>
-        </div>
-    </div>
-</div>
-
 
 <?php get_footer(); ?>
