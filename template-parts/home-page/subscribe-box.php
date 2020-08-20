d587cd766e27e0139fe7e961f9402f898e5f2368
Abhishek Verma <abhi11verma@gmail.com>
2020-08-20 13:25:50 +0530

    Fix : Subscribe box design

--- template-parts/home-page/subscribe-box.php
+++ template-parts/home-page/subscribe-box.php
@@ -5,10 +5,10 @@
 ?>
 
 <div id="subscribe-box-wrapper" class="col" style="padding:44px">
-    <div id="subscribe-box-form" class="dp-flex justify-content-center">
+    <div id="subscribe-box-form" class="dp-flex justify-content-center align-items-baseline">
         <p class="subscribe-text">Do not miss an interesting article that’s coming your way! Subscribe to our articles
             now:</p>
-        <input type="text" class="input-group-text" placeholder="Email address"/>
-        <button class="button">SUBSCRIBE
+        <input type="text" class="input-group-text subscribe-text-box" placeholder="Email address"/>
+        <button class="button subscribe-button">SUBSCRIBE
     </div>
 </div>
