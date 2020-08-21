0a0ece6536f2f020eca62d8494b4ee93bc9b5f53
Abhishek Verma <abhi11verma@gmail.com>
2020-08-21 18:13:43 +0530

    Fix : horizontal advertisement bar

--- template-parts/home-page/horizontal-advertisement.php
+++ template-parts/home-page/horizontal-advertisement.php
@@ -5,9 +5,12 @@
 ?>
 
 <div id="horizontal-advertisement-bar" class="col  dp-flex justify-content-center" style="flex:1; padding:40px" >
-    <?php if( get_field('advertisement_bar',16406) ): ?>
-        <a href="<?php the_field('advertisement_bar_link'); ?>" target="_blank" style="width: inherit">
-            <img src="<?php the_field('advertisement_bar'); ?>" width="100%"/>
+    <?php
+        $frontpage_id = get_option('page_on_front');
+        if (get_field('advertisement_bar', $frontpage_id)):
+            ?>
+        <a href="<?php the_field('advertisement_bar_link',$frontpage_id); ?>" target="_blank" style="width: inherit">
+            <img src="<?php the_field('advertisement_bar',$frontpage_id); ?>" width="100%"/>
         </a>
     <?php endif; ?>
 </div>
