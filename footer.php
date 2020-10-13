868572e0cf706862d91b757f2cc3ea5bdf6e782d
Abhishek Verma <abhi11verma@gmail.com>
2020-10-13 09:44:22 +0530

    Mobile - responsive archive page Add hyperlinks to footer logos of respective website

--- footer.php
+++ footer.php
@@ -9,21 +9,28 @@
             <?php  get_template_part('template-parts/nav-footer'); ?>
             <div id="sub-footer">
                 <div>
-                    <a><img width="100" src="<?php echo THEME_URL; ?>/images/logo.png" alt="Page Footer IJME Logo"></a>
+                    <a href="https://ijme.in"><img width="100" src="<?php echo THEME_URL; ?>/images/logo.png"
+                                                   alt="Page Footer IJME Logo"></a>
                     <div class="footer-brand-text">PUBLISHED BY FORUM FOR MEDICAL ETHICS SOCIETY SINCE 1993</div>
                     <div class="footer-brand-text">ONLINE ISSN: 0975-5091 PRINT ISSN: 0974-8466</div>
                 </div>
                 <div>
-                    <img src="<?php echo THEME_URL; ?>/images/footer/heal_logo.svg"
-                         alt="HEAL Logo">
+                    <a href="https://fmesinstitute.org/" target="_blank">
+                        <img src="<?php echo THEME_URL; ?>/images/footer/heal_logo.svg"
+                             alt="HEAL Logo">
+                    </a>
                 </div>
                 <div>
-                    <img src="<?php echo THEME_URL; ?>/images/footer/nbc_logo.svg"
-                         alt="NBC Logo">
+                    <a href="https://ijme.in/nbc-20140321/index.php" target="_blank">
+                        <img src="<?php echo THEME_URL; ?>/images/footer/nbc_logo.svg"
+                             alt="NBC Logo">
+                    </a>
                 </div>
                 <div>
-                    <img src="<?php echo THEME_URL; ?>/images/footer/fmes_logo.svg"
-                         alt="FMES Logo">
+                    <a href="https://fmesinstitute.org/" target="_blank">
+                        <img src="<?php echo THEME_URL; ?>/images/footer/fmes_logo.svg"
+                             alt="FMES Logo">
+                    </a>
                 </div>
             </div>
         </div>
@@ -31,6 +38,6 @@
         <?php
         wp_footer();
         ?>
-        
-    </body>
+
+        </body>
 </html>
