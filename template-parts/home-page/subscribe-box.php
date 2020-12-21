942b81e17062b132abb69e7ebeec8cfc20de13ed
Abhishek Verma <abhi11verma@gmail.com>
2020-12-21 15:05:49 +0530

    Change subscribe message on homepage

--- template-parts/home-page/subscribe-box.php
+++ template-parts/home-page/subscribe-box.php
@@ -6,7 +6,7 @@
 
 <div id="subscribe-box-wrapper" class="col" style="padding:44px">
     <div id="subscribe-box-form" class="dp-flex justify-content-center align-items-baseline">
-        <p class="subscribe-text">Do not miss an interesting article that’s coming your way! Subscribe to our articles
+        <p class="subscribe-text">Do not miss an interesting article that’s coming your way! Subscribe to our alerts
             now:</p>
         <input type="text" class="input-group-text subscribe-text-box" placeholder="Email address"/>
         <button class="button subscribe-button">SUBSCRIBE
