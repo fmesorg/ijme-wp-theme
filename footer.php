c707879f9a700644b6bc936dc06b23df8f12d8a5
Abhishek Verma <abhi11verma@gmail.com>
2021-01-05 11:22:50 +0530

    Add : footer icon styling and text for logos

--- footer.php
+++ footer.php
@@ -7,28 +7,33 @@
         <div class="footer">
             <?php  get_template_part('template-parts/nav-footer'); ?>
             <div id="sub-footer">
-                <div>
+                <div class="footer-brand-icon-wrapper">
                     <a href="https://ijme.in"><img width="100" src="<?php echo THEME_URL; ?>/images/logo.png"
                                                    alt="Page Footer IJME Logo"></a>
                     <div class="footer-brand-text">PUBLISHED BY FORUM FOR MEDICAL ETHICS SOCIETY SINCE 1993</div>
                     <div class="footer-brand-text">ONLINE ISSN: 0975-5091 PRINT ISSN: 0974-8466</div>
                 </div>
-                <div>
+                <div class="footer-brand-icon-wrapper">
                     <a href="https://fmesinstitute.org/" target="_blank">
                         <img src="<?php echo THEME_URL; ?>/images/footer/heal_logo.svg"
                              alt="HEAL Logo">
                     </a>
+                    <div class="footer-brand-text">Established in 2018 by FMES to pursue capacity building,</div>
+                    <div class="footer-brand-text">research, and advocacy in bioethics</div>
                 </div>
-                <div>
+                <div class="footer-brand-icon-wrapper">
                     <a href="https://ijme.in/nbc-20140321/index.php" target="_blank">
                         <img src="<?php echo THEME_URL; ?>/images/footer/nbc_logo.svg"
                              alt="NBC Logo">
+                        <div class="footer-brand-text">A biennial conference platform established by FMES in 2005</div>
                     </a>
                 </div>
-                <div>
+                <div class="footer-brand-icon-wrapper">
                     <a href="https://fmesinstitute.org/" target="_blank">
                         <img src="<?php echo THEME_URL; ?>/images/footer/fmes_logo.svg"
                              alt="FMES Logo">
+                        <div class="footer-brand-text">Working towards improving ethical standards of health care</div>
+                        <div class="footer-brand-text">and research since 1995</div>
                     </a>
                 </div>
             </div>
