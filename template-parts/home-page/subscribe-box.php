dbec3c2b0c768139bbc84cea0b53157b28a9d7ad
Abhishek Verma <abhi11verma@gmail.com>
2021-01-05 11:47:35 +0530

    Subscribe button adjustment

--- template-parts/home-page/subscribe-box.php
+++ template-parts/home-page/subscribe-box.php
@@ -7,8 +7,8 @@
 <div id="subscribe-box-wrapper" class="col" style="padding:44px">
     <div id="subscribe-box-form" class="dp-flex justify-content-center align-items-baseline">
         <p class="subscribe-text">Do not miss an interesting article that’s coming your way! Subscribe to our alerts
-            now:</p>
-        <input type="text" class="input-group-text subscribe-text-box" placeholder="Email address"/>
+            now</p>
+<!--        <input type="text" class="input-group-text subscribe-text-box" placeholder="Email address"/>-->
         <button class="button subscribe-button">SUBSCRIBE
     </div>
 </div>
