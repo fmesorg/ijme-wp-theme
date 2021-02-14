1947f6a1afc2f5bab636f9aa0fadd70100eb13f9
Abhishek Verma <abhi11verma@gmail.com>
2021-02-14 11:41:53 +0530

    Fix : overflow

--- functions.php
+++ functions.php
@@ -74,7 +74,7 @@ function enqueue_front_end_scripts()
     wp_enqueue_style('navbar-ijme', THEME_URL . '/css/navbar.css', [], '1.0.4');
     wp_enqueue_style('home-ijme', THEME_URL . '/css/home-template.css', [], '1.0.4');
     wp_enqueue_style('home-onlinefirst-ijme', THEME_URL . '/css/home-online-first.css', [], '1.0.4');
-    wp_enqueue_style('mostread-category', THEME_URL . '/css/mostread-category.css', [], '1.0.4');
+    wp_enqueue_style('mostread-category', THEME_URL . '/css/mostread-category.css', [], '1.0.5');
     wp_enqueue_style('single-article', THEME_URL . '/css/single-article.css', [], '1.0.4');
     wp_enqueue_style('single-issue', THEME_URL . '/css/single-issue.css', [], '1.0.4');
     wp_enqueue_style('nav-footer', THEME_URL . '/css/nav-footer.css', [], '1.0.4');
