a2d87c97d18a8bfa67e36aba061f827ece2f4b6c
sachin kadam <sachinkadamsk01@gmail.com>
2021-06-18 14:30:03 +0530

    add unsubscribe in html generator

--- functions.php
+++ functions.php
@@ -1947,7 +1947,7 @@ foreach ($allCategories
         <tr>
         <td style=\"margin-bottom: 50px;padding-top: 90px\">
             <span><br> <a href='{sp-browser-url}'>View it in your browser</a><br><br><a
-                    href='{sp-unsubscribe-url}'>unsubscribe from this list</a></span>
+                    </span>[sp-unsubscribe-form placeholder='Email' btntxt='Unsubscribe' ]
 
                        <a style=\"color: white; background-color: red; padding: 13px; text-decoration: none;float: right;font-size: small\" href=\"https://ijme.in/news-letter-subscription-form\" target=\"_blank\">SUBSCRIBE</a>
 
