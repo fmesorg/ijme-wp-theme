84bcbfe0501b764c2c752f8f28b9a619af133f2f
sachin kadam <sachinkadamsk01@gmail.com>
2021-08-08 23:13:00 +0530

    add cc and open access logo

--- template-parts/nav-footer.php
+++ template-parts/nav-footer.php
@@ -1,35 +1,39 @@
 <nav id="site-navigation-footer" class="" role="navigation">
     <div class="navfooter">
         <div class="navbar-footer">
-        <div class="hidden-xs hidden-sm">
+            <div class="hidden-xs hidden-sm">
+                <?php
+                wp_nav_menu(array(
+                        //'menu'              => 'primary',
+                        'theme_location' => 'footer',
+                        'depth' => 2,
+                        'container' => 'div',
+                        'container_class' => 'footer-nav-container',
+                        'container_id' => 'footer-nav-container-id',
+                        'menu_class' => 'footer-nav-menu',
+                        'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+                        'items_wrap' => '<ul id="%2$s" class="%2$s">%3$s<li><a href="https://ijme.in"><img width="150" src="http://test.ijme.in/images/cc_and_open_access_logo.png"
+                                                   alt="Page Footer IJME Logo"></a></li></ul>')
+                );
+//                wp_nav_menu(array('items_wrap' => '<ul id="%1$s" class="%2$s"><li><a href="http://www.google.com">go to google</a></li>%3$s</ul>'));
+
+                ?>
+            </div>
+
+            <!--        <div class="visible-xs visible-sm">-->
+            <!--            --><?php
+            //            wp_nav_menu( array(
+            //                    'menu'              => 'footer',
+            //                    //'theme_location'    => 'header',
+            //                    'depth'             => 1,
+            //                    'container'         => 'div',
+            //                    'container_class'   => 'footer-nav-container',
+            //                    'container_id'      => 'footer-nav-container-id',
+            //                    'menu_class'        => 'footer-nav-menu')
+            //            );
+            //            ?>
+            <!--        </div>-->
             <?php
-            wp_nav_menu( array(
-                    //'menu'              => 'primary',
-                    'theme_location'    => 'footer',
-                    'depth'             =>  2,
-                    'container'         => 'div',
-                    'container_class'   => 'footer-nav-container',
-                    'container_id'      => 'footer-nav-container-id',
-                    'menu_class'        => 'footer-nav-menu',
-                    'fallback_cb'       => 'wp_bootstrap_navwalker::fallback')
-            );
             ?>
         </div>
-
-<!--        <div class="visible-xs visible-sm">-->
-<!--            --><?php
-//            wp_nav_menu( array(
-//                    'menu'              => 'footer',
-//                    //'theme_location'    => 'header',
-//                    'depth'             => 1,
-//                    'container'         => 'div',
-//                    'container_class'   => 'footer-nav-container',
-//                    'container_id'      => 'footer-nav-container-id',
-//                    'menu_class'        => 'footer-nav-menu')
-//            );
-//            ?>
-<!--        </div>-->
-        <?php
-        ?>
-    </div>
 </nav><!-- #site-navigation -->
