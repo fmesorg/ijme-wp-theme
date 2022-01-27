ec7f8a8c5de4897cc17245fb628876913cdd2b30
sachsk <sachinkadamsk01@gmail.com>
2022-01-27 13:02:29 +0530

    minor fix in cc

--- single-articles.php
+++ single-articles.php
@@ -208,12 +208,6 @@ if (isset($_GET['galley']) && $_GET['galley'] == 'print') {
             <div id="article-abstract"><?php echo get_the_excerpt(); ?></div>
         </div>
         <div>
-//             <p></br>Copyright: @Indian Journal of Medical Ethics</br>
-//                 Published under the Creative Common license (CC BY-NC-ND 4.0)</br>
-//                 <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/"> <span
-//                             style="text-decoration: underline;color:blue">
-//                 https://creativecommons.org/licenses/by-nc-nd/4.0/</span></a>
-//             </p>
 
           <p></br>  Copyright and license </br>
             ©Indian Journal of Medical Ethics 2022: Open Access and Distributed under the Creative Commons license ( <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/"> <span
